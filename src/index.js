0350791dfe7835211449e70b7a21db888f7fab70
ibrahim karim eddin <ibrahimkarimeddin@gmail.com>
2023-02-03 20:00:40 +0300

    Remove Comment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-// import i18n from '../src/translate/Translate'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/AppStyle/Import.scss'
 import ReactDOM from 'react-dom/client';
