aca9b97138460f8963874f81d2f330f39780b84c
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-02-04 11:02:09 +0200

    ibra

--- src/Layout/Qyeri.js
+++ src/Layout/Qyeri.js
@@ -1,8 +1,9 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import BGM from '../image/Home/BGM.png'
 import BG from '../image/Home/BG.png'
 import H_M from '../image/mobile/HomeMobile.png'
-
+import H_P from '../image/Laptop/H_P.png'
+import S_P from '../image/Laptop/S_P.png'
 const Qyeri = () => {
     useEffect(() => {
         handleResize()
@@ -11,27 +12,52 @@ const Qyeri = () => {
             const Height = window.innerHeight / 100 ;
            
             console.clear()
-            if(document.body.classList[0] === "Tap"){
-                console.log("do");
-                
-            }
-            console.log(Width / Height )
             
-             if(Width / Height <= 1){
-                console.log("mom")
+            console.log(Width / Height )
+             
+            if((Width / Height <= 1) && (Width / Height >= .7) ){
+                console.log("As")
+                document.body.classList.add("Tap")
 
+                document.body.classList.add("BigTap")
+                document.getElementById("Laptop1").src = H_M ;
+                document.getElementById("Laptop2").src = H_M ;
+                document.getElementById("Laptop3").src = H_M ;
+                document.getElementById("Laptop4").src = H_M ;
 
             }
-            if(Width / Height <= 1.2 ){
+            
+            else if(Width / Height <= 1.2 ){
                 console.log("sos")
                 document.body.classList.add("Tap")
-                document.body.classList.remove("Lap")
+                document.body.classList.remove("BigTap")
                 document.getElementById("BG").src= BGM 
+                document.getElementById("Laptop1").src = H_P ;
+                    document.getElementById("Laptop2").src = S_P ;
+                    document.getElementById("Laptop3").src = H_P ;
+                    document.getElementById("Laptop4").src = S_P ;
             }
+            else if(Width / Height <= 1.5 ){
+                document.body.classList.remove("BigTap")
+                document.body.classList.remove("Tap")
+                document.body.classList.add("BigLap")
+
+                document.getElementById("BG").src= BG 
+                document.getElementById("Laptop1").src = H_M ;
+                document.getElementById("Laptop2").src = H_M ;
+                document.getElementById("Laptop3").src = H_M ;
+                document.getElementById("Laptop4").src = H_M ;
+            }
+           
            
             else{
                 document.body.classList.remove("Tap")
                 document.getElementById("BG").src= BG 
+                document.body.classList.add("BigTap")
+                    document.getElementById("Laptop1").src = H_P ;
+                    document.getElementById("Laptop2").src = S_P ;
+                    document.getElementById("Laptop3").src = H_P ;
+                    document.getElementById("Laptop4").src = S_P ;
 
             }
 
