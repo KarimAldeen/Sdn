a06f1e9f2dd14c70a9369140a117906e9a2b935c
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-02-10 13:24:42 +0200

    news

--- src/Layout/Const/Navbar.js
+++ src/Layout/Const/Navbar.js
@@ -8,9 +8,9 @@ const Navbar = () => {
         <div className='Navbar absolute'>
             <div className='Links'>
                 <Link to="/" className="Link">Home</Link>
-                <Link to="services" className="Link">Servises</Link>
-                <Link to="Team" className="Link">Our Team</Link>
-                <Link to="Contact" className="Link">Contact</Link>
+                <Link to="/services" className="Link">Servises</Link>
+                <Link to="/Team" className="Link">Our Team</Link>
+                <Link to="/Contact" className="Link">Contact</Link>
             </div>
 
             <div className='Tools'>
--- src/components/Services/Services_MidSec.js
+++ src/components/Services/Services_MidSec.js
@@ -1,32 +1,14 @@
 import React from 'react'
-import Img from '../../image/Services/1.svg'
-import Img2 from '../../image/Services/2.svg'
-import Img3 from '../../image/Services/3.svg'
-import Img4 from '../../image/Services/4.svg'
+import Img from '../../image/Services/Service.svg'
+
 
 const Services_MidSec = () => {
   return (
     <div className='Services_Mid' id='Services_Mid'>
-              <div className='Mid_Items'>
 
       <div className='Mid_Item' >
           <img src={Img} id='Mid_Item'  alt='' />
         </div>
-        <div className='Mid_Item Mid_Items2'>
-        <img src={Img2} alt='' />
-
-        </div>
-      </div>
-      <div className='Mid_Items Mid_Items2'>
-        <div className='Mid_Item'>
-        <img src={Img3} alt='' />
-
-        </div>
-        <div className='Mid_Item'>
-        <img src={Img4} alt='' />
-
-        </div>
-      </div>
 
 
 
