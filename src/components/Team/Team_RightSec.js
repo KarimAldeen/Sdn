6e772dfadb7dbcc34e54543a8be8425cdfde7051
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-02-28 13:56:33 +0200

    klb

--- src/components/Team/Team_RightSec.js
+++ src/components/Team/Team_RightSec.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Team from "../../image/Team/Team.png"
-
+// import Team from '../../image/Team/TeamSec.png'
 const Team_RightSec = () => {
     return (
         <>   
